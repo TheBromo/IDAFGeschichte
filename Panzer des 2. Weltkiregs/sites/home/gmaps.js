f6bb21856261f7eba7681a633afac1f6c3e204cb
shadowsburney <shadowsburney@gmail.com>
2018-04-05 23:40:29 +0200

    Added distances

--- Panzer des 2. Weltkiregs/sites/home/gmaps.js
+++ Panzer des 2. Weltkiregs/sites/home/gmaps.js
@@ -1,4 +1,5 @@
 var map;
+
 function initMap() {
 
     map = new google.maps.Map(document.getElementById('map'), {
@@ -180,121 +181,104 @@ function initMap() {
             position: new google.maps.LatLng(50.232399, 10.417957),
             icon: 'icons/panther.png',
             name: 'Panzer V - Panther',
-            info: '<div class="content">'+
-            '<h1 class="title">Panzer V - Panther</h1>'+
+            info: '<div class="content">' +
+            '<h1 class="title">Panzer V - Panther</h1>' +
             '<span class="origin"><img src="../pictures/NSDAP_flag.png"><p>- Deutsches Reich</p></span>' +
             '<div class="body-content">' +
-            '<a href="../panther/panther.html">Mehr zum <b>Panzer V - Panther<b></a>'+
-            '</div>'+
+            '<a href="../panther/panther.html">Mehr zum <b>Panzer V - Panther<b></a>' +
+            '</div>' +
             '</div>',
             usage: [
-                new google.maps.LatLng(53.9377,11.10915),
-                new google.maps.LatLng(54.41039,12.87678),
-                new google.maps.LatLng(53.80176,14.38075),
-                new google.maps.LatLng(54.74483,18.00391),
-                new google.maps.LatLng(54.32431,19.27367),
-                new google.maps.LatLng(55.16176,21.02217),
-                new google.maps.LatLng(56.2055,20.77581),
-                new google.maps.LatLng(57.58618,21.38988),
-                new google.maps.LatLng(59.27749,24.06938),
-                new google.maps.LatLng(59.84665,29.62613),
-                new google.maps.LatLng(59.99564,31.26477),
-                new google.maps.LatLng(56.72034,35.60008),
-                new google.maps.LatLng(55.80997,37.24528),
-                new google.maps.LatLng(54.77665,38.06908),
-                new google.maps.LatLng(52.47545,39.89003),
-                new google.maps.LatLng(50.30043,40.09257),
-                new google.maps.LatLng(47.31093,39.54805),
-                new google.maps.LatLng(45.65709,29.2305),
-                new google.maps.LatLng(47.38769,25.20503),
-                new google.maps.LatLng(48.22123,21.83875),
-                new google.maps.LatLng(48.05939,17.87473),
-                new google.maps.LatLng(47.48235,15.98918),
-                new google.maps.LatLng(47.65173,12.96105),
-                new google.maps.LatLng(47.2569,9.69123),
-                new google.maps.LatLng(47.69943,7.80124),
-                new google.maps.LatLng(47.72751,3.79105),
-                new google.maps.LatLng(48.54716,-1.05409),
-                new google.maps.LatLng(49.58078,-1.77953),
-                new google.maps.LatLng(49.87335,0.72467),
-                new google.maps.LatLng(51.0565,2.61363),
-                new google.maps.LatLng(53.07707,5.61642),
-                new google.maps.LatLng(53.42703,7.82819),
+                new google.maps.LatLng(53.9377, 11.10915),
+                new google.maps.LatLng(54.41039, 12.87678),
+                new google.maps.LatLng(53.80176, 14.38075),
+                new google.maps.LatLng(54.74483, 18.00391),
+                new google.maps.LatLng(54.32431, 19.27367),
+                new google.maps.LatLng(55.16176, 21.02217),
+                new google.maps.LatLng(56.2055, 20.77581),
+                new google.maps.LatLng(57.58618, 21.38988),
+                new google.maps.LatLng(59.27749, 24.06938),
+                new google.maps.LatLng(59.84665, 29.62613),
+                new google.maps.LatLng(59.99564, 31.26477),
+                new google.maps.LatLng(56.72034, 35.60008),
+                new google.maps.LatLng(55.80997, 37.24528),
+                new google.maps.LatLng(54.77665, 38.06908),
+                new google.maps.LatLng(52.47545, 39.89003),
+                new google.maps.LatLng(50.30043, 40.09257),
+                new google.maps.LatLng(47.31093, 39.54805),
+                new google.maps.LatLng(45.65709, 29.2305),
+                new google.maps.LatLng(47.38769, 25.20503),
+                new google.maps.LatLng(48.22123, 21.83875),
+                new google.maps.LatLng(48.05939, 17.87473),
+                new google.maps.LatLng(47.48235, 15.98918),
+                new google.maps.LatLng(47.65173, 12.96105),
+                new google.maps.LatLng(47.2569, 9.69123),
+                new google.maps.LatLng(47.69943, 7.80124),
+                new google.maps.LatLng(47.72751, 3.79105),
+                new google.maps.LatLng(48.54716, -1.05409),
+                new google.maps.LatLng(49.58078, -1.77953),
+                new google.maps.LatLng(49.87335, 0.72467),
+                new google.maps.LatLng(51.0565, 2.61363),
+                new google.maps.LatLng(53.07707, 5.61642),
+                new google.maps.LatLng(53.42703, 7.82819),
             ]
         },
         sherman: {
             position: new google.maps.LatLng(52.130798, -1.586160),
             icon: 'icons/sherman.png',
             name: 'Sherman Firefly',
-            info: '<div class="content">'+
-            '<h1 class="title">Sherman Firefly</h1>'+
+            info: '<div class="content">' +
+            '<h1 class="title">Sherman Firefly</h1>' +
             '<span class="origin"><img src="../pictures/uk_flag.png"><p>- Vereinigtes Königreich</p></span>' +
             '<div class="body-content">' +
-            '<a href="../sherman/sherman.html">Mehr zum <b>Sherman Firefly<b></a>'+
-            '</div>'+
+            '<a href="../sherman/sherman.html">Mehr zum <b>Sherman Firefly<b></a>' +
+            '</div>' +
             '</div>',
             usage: [
-                new google.maps.LatLng(53.84706,11.3069),
-                new google.maps.LatLng(53.82189,9.61836),
-                new google.maps.LatLng(53.38204,6.69134),
-                new google.maps.LatLng(52.93585,5.01909),
-                new google.maps.LatLng(52.13573,4.55533),
-                new google.maps.LatLng(51.07862,3.13977),
-                new google.maps.LatLng(49.98391,4.44285),
-                new google.maps.LatLng(47.89724,8.43913),
-                new google.maps.LatLng(47.45933,9.81225),
-                new google.maps.LatLng(46.91204,10.71034),
-                new google.maps.LatLng(46.28322,11.45462),
-                new google.maps.LatLng(45.93056,12.5175),
-                new google.maps.LatLng(45.85179,13.44855),
-                new google.maps.LatLng(47.26881,12.0859),
-                new google.maps.LatLng(47.85591,12.48072),
-                new google.maps.LatLng(49.15432,12.5233),
-                new google.maps.LatLng(50.34146,12.01046),
-                new google.maps.LatLng(52.72746,11.36578),
+                new google.maps.LatLng(53.29601, 5.71624), new google.maps.LatLng(49.93775, 1.60512), new google.maps.LatLng(49.52165, -1.92372), new google.maps.LatLng(49.3178, -1.79644), new google.maps.LatLng(49.1131, -1.58126), new google.maps.LatLng(47.221, 1.54757), new google.maps.LatLng(46.54322, 5.99476), new google.maps.LatLng(47.21709, 6.83843), new google.maps.LatLng(47.50635, 6.86476), new google.maps.LatLng(47.60596, 8.28417), new google.maps.LatLng(47.84774, 8.55443), new google.maps.LatLng(47.49529, 9.76621), new google.maps.LatLng(47.13912, 9.63602), new google.maps.LatLng(46.86224, 10.1885), new google.maps.LatLng(46.92263, 10.66407), new google.maps.LatLng(46.65577, 10.45965), new google.maps.LatLng(47.65327, 13.01281), new google.maps.LatLng(48.69737, 13.86308), new google.maps.LatLng(49.98758, 11.90418), new google.maps.LatLng(50.87549, 14.05034), new google.maps.LatLng(51.49312, 13.10469), new google.maps.LatLng(51.69621, 12.20979), new google.maps.LatLng(52.11817, 11.54561), new google.maps.LatLng(52.5078, 11.94209), new google.maps.LatLng(53.00331, 11.52108), new google.maps.LatLng(53.84482, 9.23983), new google.maps.LatLng(53.72536, 8.03366),
             ]
         },
         t34: {
             position: new google.maps.LatLng(51.345429, 24.717918),
             icon: 'icons/t34.png',
             name: 'T34-85',
-            info: '<div class="content">'+
-            '<h1 class="title">T34-85</h1>'+
+            info: '<div class="content">' +
+            '<h1 class="title">T34-85</h1>' +
             '<span class="origin"><img src="../pictures/soviets_flag.png"><p>- Sowjetunion</p></span>' +
             '<div class="body-content">' +
-            '<a href="../t34/t34.html">Mehr zum <b>T34-85<b></a>'+
-            '</div>'+
+            '<a href="../t34/t34.html">Mehr zum <b>T34-85<b></a>' +
+            '</div>' +
             '</div>',
             usage: [
-                new google.maps.LatLng(53.88593,11.32888),
-                new google.maps.LatLng(54.35921,13.09651),
-                new google.maps.LatLng(53.74982,14.60047),
-                new google.maps.LatLng(54.69407,18.22363),
-                new google.maps.LatLng(54.27302,19.49339),
-                new google.maps.LatLng(55.11152,21.24189),
-                new google.maps.LatLng(56.15658,20.99554),
-                new google.maps.LatLng(57.53904,21.60961),
-                new google.maps.LatLng(59.23256,24.28911),
-                new google.maps.LatLng(59.80247,29.84586),
-                new google.maps.LatLng(59.95166,31.4845),
-                new google.maps.LatLng(56.67208,35.81981),
-                new google.maps.LatLng(55.76055,37.46501),
-                new google.maps.LatLng(54.72592,38.28881),
-                new google.maps.LatLng(52.42189,40.10975),
-                new google.maps.LatLng(50.24425,40.3123),
-                new google.maps.LatLng(47.25131,39.76778),
-                new google.maps.LatLng(45.59563,29.45022),
-                new google.maps.LatLng(47.32816,25.42476),
-                new google.maps.LatLng(48.16264,22.05847),
-                new google.maps.LatLng(47.85338,17.83079),
-                new google.maps.LatLng(46.94505,16.03313),
-                new google.maps.LatLng(45.50754,14.41125),
-                new google.maps.LatLng(45.89769,13.47052),
-                new google.maps.LatLng(47.31352,12.10788),
-                new google.maps.LatLng(47.90012,12.5027),
-                new google.maps.LatLng(49.19741,12.54527),
-                new google.maps.LatLng(50.38352,12.03243),
-                new google.maps.LatLng(52.76736,11.38776),
+                new google.maps.LatLng(53.88593, 11.32888),
+                new google.maps.LatLng(54.35921, 13.09651),
+                new google.maps.LatLng(53.74982, 14.60047),
+                new google.maps.LatLng(54.69407, 18.22363),
+                new google.maps.LatLng(54.27302, 19.49339),
+                new google.maps.LatLng(55.11152, 21.24189),
+                new google.maps.LatLng(56.15658, 20.99554),
+                new google.maps.LatLng(57.53904, 21.60961),
+                new google.maps.LatLng(59.23256, 24.28911),
+                new google.maps.LatLng(59.80247, 29.84586),
+                new google.maps.LatLng(59.95166, 31.4845),
+                new google.maps.LatLng(56.67208, 35.81981),
+                new google.maps.LatLng(55.76055, 37.46501),
+                new google.maps.LatLng(54.72592, 38.28881),
+                new google.maps.LatLng(52.42189, 40.10975),
+                new google.maps.LatLng(50.24425, 40.3123),
+                new google.maps.LatLng(47.25131, 39.76778),
+                new google.maps.LatLng(45.59563, 29.45022),
+                new google.maps.LatLng(47.32816, 25.42476),
+                new google.maps.LatLng(48.16264, 22.05847),
+                new google.maps.LatLng(47.85338, 17.83079),
+                new google.maps.LatLng(46.94505, 16.03313),
+                new google.maps.LatLng(45.50754, 14.41125),
+                new google.maps.LatLng(45.89769, 13.47052),
+                new google.maps.LatLng(47.31352, 12.10788),
+                new google.maps.LatLng(47.90012, 12.5027),
+                new google.maps.LatLng(49.19741, 12.54527),
+                new google.maps.LatLng(50.38352, 12.03243),
+                new google.maps.LatLng(52.76736, 11.38776),
             ]
         }
     };
@@ -336,7 +320,7 @@ function initMap() {
         });
 
         // Info
-        marker.addListener('click', function() {
+        marker.addListener('click', function () {
             infowindow.open(map, marker);
             tank.heatmap.setMap(map);
             for (var t in tanks) {
@@ -360,5 +344,6 @@ function initMap() {
         });
         tank.heatmap = heatmap;
     }
+
     map.controls[google.maps.ControlPosition.LEFT_TOP].push(legend);
 }
